76b4a2aec8ed19ced6390ee41b8f7d4a3037c0c2
MIYUISHITSUBO <miyu@k-nsi.co.jp>
2025-09-22 02:08:28 +0900

    きっとうまくいく

--- pages/collections/25ss/volata.vue
+++ pages/collections/25ss/volata.vue
@@ -3,28 +3,108 @@
       <div class="iframecontainer">
         <iframe src="https://www.youtube.com/embed/lsAOykqZNRs?si=aST6gukrnBT7oiGh&autoplay=1&controls=0&disablekb=1&loop=1&mute=0&playsinline=1&rel=0" title="YouTube video player" frameborder="0"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
+      <div class="flexboxa">
+        <h2>
+          <a href="https://volata.jp/" target="_blank">VOLATA</a>
+        </h2>
+      </div>
       <div class="flexbox">
         <div class="box">
           <div class="t-img">
-            <img class="mainimg" alt="mitchdesunia" src="@/assets/img/volata/batch_DSC06155.jpg" data-index="1"/>
+            <img class="mainimg" alt="volata" src="/img/volata/1.jpg" data-index="1"/>
             <div class="t-info">
             </div>
           </div>          
         </div>
         <div class="box">
           <div class="t-img">
-            <img class="mainimg" alt="mitchdesunia" src="@/assets/img/volata/batch_DSC06175.jpg" data-index="1"/>
+            <img class="mainimg" alt="volata" src="/img/volata/2.jpg" data-index="1"/>
             <div class="t-info">
             </div>
           </div>          
         </div>
-
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/3.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/4.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/5.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/6.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/7.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>        
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/11.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/8.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/9.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/10.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+        <div class="box">
+          <div class="t-img">
+            <img class="mainimg" alt="volata" src="/img/volata/0.jpg" data-index="1"/>
+            <div class="t-info">
+            </div>
+          </div>          
+        </div>
+            <div id="modal" class="modal" style="display: none;">
+                <span class="close">&times;</span>
+                <img class="modal-content" id="modalImage">
+                <div class="nav prev">&#10094;</div>
+                <div class="nav next">&#10095;</div>
+            </div>
       </div>
     <footern/>
 </template>
 <script setup>
 useHead({
-  title: "VOLATA 2025 SPRING/SUMMER | Japan Runway Show",
+  title: "VOLATA 2025 SPRING | Japan Runway Show",
   script: [
     {
       src: '/js/script.js',
@@ -32,8 +112,8 @@ useHead({
       body: true, 
     }],
  meta: [
-    {name: 'description', content: 'VOLATA | Japan Runway Show'},
-    {property: 'og:title', content: 'VOLATA | Japan Runway Show'},
+    {name: 'description', content: 'VOLATA 2025 SPRINGコレクションを今すぐチェック'},
+    {property: 'og:title', content: 'VOLATA 2025 SPRINGコレクションを今すぐチェック'},
  ]
 });
 
