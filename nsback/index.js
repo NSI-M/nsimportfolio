f65173bc3d8337b8fc79550424081c7b14de1459
MIYUISHITSUBO <miyu@k-nsi.co.jp>
2025-11-13 14:38:22 +0900

    チケット種類追加

--- nsback/index.js
+++ nsback/index.js
@@ -375,6 +375,85 @@ app.post('/api/auth/create-checkout-session6', async (req, res) => {
   res.send({clientSecret: session.client_secret});
 });
 
+//企業モデル用のアフターパーティチケット
+app.post('/api/auth/create-checkout-session7', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: 'embedded',
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price: 'price_1SSrvl09dtrC0gbU5WypJhkF',
+        quantity: 1,
+      },
+    ],
+    consent_collection: {
+        terms_of_service: 'required',
+    },
+    //metadata: {
+    //  agreed_to_terms: agreed.toString()
+    //},
+    mode: 'payment',
+    return_url: `${process.env.FRONTEND_ORIGIN}/success`,
+    automatic_tax: {enabled: true},
+  });
+  console.log(`ええかんじやね`)
+  res.send({clientSecret: session.client_secret});
+});
+
+
+//申し込みモデル用のチケット
+app.post('/api/auth/create-checkout-session8', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: 'embedded',
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price: 'price_1SSrtF09dtrC0gbUKvACFfeN',
+        quantity: 1,
+      },
+    ],
+    consent_collection: {
+        terms_of_service: 'required',
+    },
+    //metadata: {
+    //  agreed_to_terms: agreed.toString()
+    //},
+    mode: 'payment',
+    return_url: `${process.env.FRONTEND_ORIGIN}/success`,
+    automatic_tax: {enabled: true},
+  });
+  console.log(`ええかんじやね`)
+  res.send({clientSecret: session.client_secret});
+});
+
+
+//ブランドモデル用のチケット
+app.post('/api/auth/create-checkout-session9', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: 'embedded',
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price: 'price_1SSsyN09dtrC0gbULJ5etyeB',
+        quantity: 1,
+      },
+    ],
+    consent_collection: {
+        terms_of_service: 'required',
+    },
+    //metadata: {
+    //  agreed_to_terms: agreed.toString()
+    //},
+    mode: 'payment',
+    return_url: `${process.env.FRONTEND_ORIGIN}/success`,
+    automatic_tax: {enabled: true},
+  });
+  console.log(`ええかんじやね`)
+  res.send({clientSecret: session.client_secret});
+});
+
+
+
 
 //多分ここまででよい
 
---
+++ pages/auditiontickets/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <Headertop/>
+    <topvideo/>
+    <div class="pflexbox">
+        <h2>
+            <span style="font-family: 'Orbitron';">Tickets</span>
+        </h2>
+        <p>
+            OPEN12:00/START13:00/PARTY18:00~20:00<br>ご参加の際はご購入ページ記載の利用規約の確認をお願いいたします。外部団体の撮影に関する同意が必要です。<br>アフターパーティつきご観覧チケットでは、ショー終了後、次世代の基幹事業と目される「クリエイティブ・エコノミー」の興隆を期し、出展ブランドとクリエイターによるアフターパーティにご参加いただけます。
+        </p>
+        <div class="newsitem">
+            <a href="checkout_model" style="display: flex;">
+                <div class="newsimg">
+                    <img src="@/assets/img/thumbdef.png"/>
+                </div>
+                <div class="newsinfo" style="justify-content: center;">
+                    <h3>ご観覧チケット<br>￥5,000</h3>
+                </div>
+            </a>
+        </div>
+
+    </div>
+    <footern/>
+</template>
+<script>
+import { NuxtLink } from '#components';
+import { ref, onMounted } from 'vue';
+
+// 1. useHeadでSwiperのCDNリソースを読み込む
+useHead({
+  title: "チケット一覧",
+  script: [
+    {
+      src: '/js/swiper-bundle.min.js',
+      // body: true をつけると<body>の末尾で読み込まれる
+      body: true, 
+    }],
+ meta: [
+    {name: 'description', content: 'チケット一覧 | Japan Runway Show'},
+    {property: 'og:title', content: 'チケット一覧 | Japan Runway Show'},
+    {property:"twitter:description", content:"チケット一覧 | Japan Runway Show"},
+    {name:"robots", content:"index, nofollow, max-image-preview:large"}
+ ]
+})
+</script>
